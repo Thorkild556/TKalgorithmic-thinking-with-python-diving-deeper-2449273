19e5bda8d1402ac4165a3fed08e824ef454ed93b
Thorkild556 <thorkild1234@gmail.com>
2023-11-02 18:04:15 +0100

    now works in the given format

--- 01_01/two_sum_stub.py
+++ 01_01/two_sum_stub.py
@@ -2,21 +2,29 @@
 
 def two_sum_problem(arr, target):
     list = []
+    x = -1
     for i in arr:
+        y = 0
+        x+=1
         for n in arr:
-            if i + n == target and i != n:
-                sub_list = i-1, n-1
-                list.append(sub_list)
-    return list
+            if i + n == target and x != y:
+                sub_list = x, y
+                #list.append(sub_list)
+            else:
+                y +=1
+    return sub_list #list.append(sub_list)
                 
-arr = [1, 2, 3]
 
-target = 4
+
+assert two_sum_problem([1, 2, 3], 4) in [(0, 2), (2, 0)]
+assert two_sum_problem([1234, 5678, 9012], 14690) in [(1, 2), (2, 1)]
+assert two_sum_problem([2, 2, 3], 4) in [(0, 1), (1, 0)]
+assert two_sum_problem([2, 2], 4) in [(0, 1), (1, 0)]
+assert two_sum_problem([8, 7, 2, 5, 3, 1], 10) in [(0, 2), (2, 0), (1, 4), (4, 1)]
+
+
+arr = [1234, 5678, 9012]
+
+target = 14690
 
 print(two_sum_problem(arr, target))
-
-# assert two_sum_problem([1, 2, 3], 4) in [(0, 2), (2, 0)]
-# assert two_sum_problem([1234, 5678, 9012], 14690) in [(1, 2), (2, 1)]
-# assert two_sum_problem([2, 2, 3], 4) in [(0, 1), (1, 0)]
-# assert two_sum_problem([2, 2], 4) in [(0, 1), (1, 0)]
-# assert two_sum_problem([8, 7, 2, 5, 3, 1], 10) in [(0, 2), (2, 0), (1, 4), (4, 1)]
